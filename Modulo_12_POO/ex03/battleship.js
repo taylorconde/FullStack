556df92ef2b8a5dac71300e28f8e33e3bf496351
taylorconde <taylorconde@gmail.com>
2023-12-27 20:20:07 -0300

    cleaning branch, removing other modules files

--- Modulo_12_POO/ex03/battleship.js
+++
@@ -1,9 +0,0 @@
-import { Spaceship } from "./spaceship.js";
-
-export class Battleship extends Spaceship {
-    constructor(_name, _crewQuantity, _weaponsQuantity) {
-        super(_name, _crewQuantity);
-        this._weaponsQuantity = _weaponsQuantity;
-        this._type = "Battle";
-    }
-};
