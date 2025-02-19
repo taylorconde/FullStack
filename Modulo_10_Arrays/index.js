fd6e45d14d4ae540f4852b4f8a674c3ff092407d
taylorconde <taylorconde@gmail.com>
2025-02-18 23:17:17 -0300

    exercicio objetos

--- Modulo_10_Arrays/index.js
+++ Modulo_10_Arrays/index.js
@@ -1,39 +1,58 @@
-const hitchedSpaceships = [
-    ["Fenix", 8, true],
-    ["Golias", 10, true],
-    ["Helmet", 5, false],
-    ["Elemental", 3, true],
-    ["Darwin", 15, false],
-]
+/*
+Nome, tipo e velocidade maxima
+velocidade inicial = 0
+menu para acelerar e parar
+    se parar, 
+        informar nome, tipo e velocidade
+    se acelerar, 
+        perguntar quanto quer acelerar, velocidade sera incrementada
+            se ultrapassar velocidade maxia, exiba mensagem informando vel atual e vel maxima permitida
+prompt nome, tipo, vel maxima, acelerar ou parar.
+criar um objeto para a nave. */
 
 
-//retornar O nome das naves que tem mais de 9 tripulantes 
+let spaceship = [{
+    nome: "",
+    type: "",
+    maxSpeed: "",
+    velocity: 0,
+    start: function () {
+        this.nome = prompt("Informe o nome da nave: ");
+        this.type = prompt("Informe o tipo da nave: ");
+        this.maxSpeed = prompt("Informe a velocidade maxima da nave: ");
+        this.movement();
+    },
+    movement: function () {
+        let movement = "";
 
-let nameNine = hitchedSpaceships 
-    .filter((elemento) => elemento[1] > 9)
-    .map((elemento) => elemento[0])
-    .join(" e ");
+        do {
+            if (this.velocity == 0) {
+                movement = prompt(`Deseja partir, ou permanecer parado?\nVelocidade atual: ${this.velocity}Km/h\nPartir - (1)\nPermanecer parado - (2)`);
+            } else {
+                movement = prompt(`Deseja Acelerar, ou parar a nave?\nVelocidade atual: ${this.velocity}Km/h\nPartir - (1)\nPermanecer parado - (2)`);
+            };
 
-// alert( `As naves com mais de 9 tripulantes sao ${nameNine}`);
+            if (movement == "2" && this.velocity == 0) {
+                console.log(`Permanecer parado!\nNome da Nave: ${this.nome}\nTipo de Nave: ${this.type}\nVelocidade: ${this.velocity}Km/h`)
+            } else if (movement == "2") {
+                console.log(`Parando a nave.\nNome da Nave: ${this.nome}\nTipo de Nave: ${this.type}\nVelocidade: ${this.velocity}Km/h`)
+            } else if (movement == "1") {
+                let accelerate = parseFloat(prompt("Quanto deseja acelerar?"))
+                if (this.velocity >= this.maxSpeed) {
+                    console.log(`Velocidade máxima alcançada, reduzindo 10% da velocidade atual: \nVelocidade atual: ${this.velocity}Km/h\nReduzindo...\n..\n.`);
+                    this.velocity *= 0.9;
+                    console.log(`Velocidade reduzida para: ${this.velocity}Km/h`)
+                } else {
+                    this.velocity += accelerate;
+                    console.log(`Acelerando...\nVelocidade atual: ${this.velocity}Km/h`)
+                }
+            } else {
+                console.log("Opção inválida!\nDigite '1' Para acelerar, e '2' Para parar.")
+            }
+        } while (movement !== "2");
+    },
+}];
 
-// Informar o numero da plataforma em que esta a primeira nave que ainda esta com engate pendente
 
-let findEngateIndex = hitchedSpaceships
-    .findIndex((elemento) => !elemento[2]);
 
-let numeroPlataforma = findEngateIndex !== -1 ? findEngateIndex + 1 : "Nenhuma nave com engate pendente encontrada";
-
-// alert (`A plataforma de numero ${numeroPlataforma}, esta com engate pendente.`);
-
-// Destacar o nome de todas as naves colocando-as em caixa alta e exibindo.
-
-let upperCaseNames = hitchedSpaceships
-    .map((elemento) => elemento[0].toUpperCase());
-    
-// alert(`Nome das Naves: \n${upperCaseNames.join(",\n")}`);
-
-// Exibir um alerta com todas estas informacoes
-let mensagem = `As naves com mais de 9 tripulantes sao ${nameNine}\n`;
-mensagem += `A plataforma de numero ${numeroPlataforma}, esta com engate pendente.\n`;
-mensagem += `Nome das Naves: \n${upperCaseNames.join(",\n")}`;
-alert(mensagem);
+start = spaceship[0].start();
