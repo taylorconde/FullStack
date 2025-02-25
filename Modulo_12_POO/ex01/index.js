bdaae267dfe54e0c6268ef7c10027edde1f934f9
taylorconde <taylorconde@gmail.com>
2025-02-24 22:57:41 -0300

    ex01 POO - Classes

--- Modulo_12_POO/ex01/index.js
+++ Modulo_12_POO/ex01/index.js
@@ -1,49 +1,79 @@
-class Spaceship {
-    constructor(name, tripulationNumber) {
-        this.name = name;
-        this.tripulationNumber = tripulationNumber;
-        this.hitched = false;
-        this.dors = false;
-    }
-    engate() {
-        this.hitched = true;
-        this.dors = true;
-    }
-};
-//lista de naves
+/*
+Estação com lista de naves engatadas
+Cada nave possue 
+    nome
+    qtdd de tripulantes
+    engatada ou nao?
+    portas de entrada abertas?
+MENU:
+    1- REALIZAR O ENGATE
+        cadastrar nave com 
+            nome
+            nmr de tripulantes
+        apos o cadastro a nave PODE ser engatada
+        quando engatada, suas portas sao abertas
+    2- IMPRIMIR AS NAVES
+    3- SAIR DO PROGRAMA
+
+1 classe para criar as naves. com método para engatar a nave.
+1 array para armazenar as espaçonaves
+validar a opção de saída- somente se digitar '3'.
+*/
+
+//array com todas as naves
 let spaceships = [];
 
-// menu 
-let menuOption;
-let otherSpaceship;
+class Spaceship {
+    constructor(name, crew) {
+        this.name = name;
+        this.crew = crew;
+        this.isEngaged = false;
+        this.doorOpen = false;
+    };
 
-// functions
-function printSpaceships(spaceships) {
-    let spaceshipsList = "";
-    spaceships.forEach((spaceship, index) => {
-        spaceshipsList += `${(index + 1)} - ${spaceship.name} (${spaceship.tripulationNumber} tripulantes)\n`
-    });
-    alert(spaceshipsList);
-}
+    engaged() {
+        this.isEngaged = true;
+        this.doorOpen = true;
+    };
 
+    spaceshipsAdd(newSpaceship) {
+        spaceships.push(newSpaceship);
+    };
+};
 
+function registeringSpaceship() {
+
+    let name = prompt("Informe o nome da Nave: ");
+    let quantityCrew = prompt("Informe a quantidade de tripulantes: ");
+    let spaceship = new Spaceship(name, quantityCrew);
+    spaceship.engaged();
+    spaceship.spaceshipsAdd(spaceship);
+    return spaceship;
+};
+
+function spaceshipsInformation() {
+
+    let listSpaceships = spaceships.map((spaceship, index) => {
+        return `${index + 1} - Spaceship  -  Name: ${spaceship.name}, Crew: ${spaceship.crew} -\n`
+    }).join("");
+    alert(listSpaceships);
+};
+
+let menu;
 do {
+    menu = prompt(`-STARSHIPS MENU-\nREALIZAR O ENGATE (1)\nIMPRIMIR AS NAVES (2)\nSAIR DO PROGRAMA (3)`);
 
-    menuOption = prompt("1-Realizar engate\n2-Imprimir naves\n3-Sair do programa");
-
-    switch(menuOption) {
+    switch (menu) {
         case "1":
-            let newName = prompt("Informe o nome da nave");
-            let numTripulantes = prompt("Informe o numero de tripulantes");
-            let otherSpaceship = new Spaceship(newName, numTripulantes);
-            otherSpaceship.engate();
-            spaceships.push(otherSpaceship);
+            registeringSpaceship();
             break;
         case "2":
-            printSpaceships(spaceships);
+            spaceshipsInformation();
             break;
         case "3":
-            alert("Encerrando o sistema...");
+            alert("Saindo do programa")
+            break;
+        default:
+            alert("Digite apenas o número de cada opção, (1,2,3)")
     }
-
-} while (menuOption != "3");
+} while (menu !== "3")
