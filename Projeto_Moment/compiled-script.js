97dbd31b060cd8687f75e5533fdd705907036b41
taylorconde <taylorconde@gmail.com>
2025-03-10 19:37:20 -0300

    Transpilando com Babel
    - Utilizando Node.js
    - Utilizando npm
    - Transpilando com Babel

---
+++ Projeto_Moment/compiled-script.js
@@ -0,0 +1,4 @@
+"use strict";
+
+var today = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
+alert("Today is ".concat(today));
---
+++ Projeto_Moment/script.js
@@ -0,0 +1,2 @@
+const today = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
+alert(`Today is ${today}`);
