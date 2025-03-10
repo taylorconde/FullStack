46e026494ba96256a4365256bb4d3c669880a0c7
taylorconde <taylorconde@gmail.com>
2025-03-10 20:16:15 -0300

    Acidionando corejs ao babel
    - Versão 3.41.0 do corejs.

--- Projeto_Moment/babel.config.js
+++ Projeto_Moment/babel.config.js
@@ -1,3 +1,13 @@
-module.exports = {
-    presets: ["@babel/preset-env"],
-};
+const { version } = require("core-js")
+
+const presets = [
+    [
+        "@babel/preset-env",
+        {
+            useBuiltIns: "usage",
+            corejs: "3.41.0"
+        }
+    ]
+]
+
+module.exports = { presets }
--- Projeto_Moment/script.js
+++ Projeto_Moment/script.js
@@ -1,2 +1,4 @@
 const today = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
 alert(`Today is ${today}`);
+
+let promise = Promise.resolve()
