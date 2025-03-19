32b89e2b83181898a2278cfc1ebc925185f34838
taylorconde <taylorconde@gmail.com>
2025-03-19 20:39:17 -0300

    Updating async functions
    - Removing Promises
    - Adding return/throw new Error

--- ex01/project/src/engine.js
+++ ex01/project/src/engine.js
@@ -8,36 +8,29 @@ export class SpacechipEngine {
 
     async turnOn() {
 
-        // Cria uma Promise que resolve ou rejeita
-        return new Promise((resolve, reject) => {
-            const percent = this.spaceship.percent();
+        const percent = this.spaceship.percent();
 
-            if (percent >= 30) {
-                this.isOn = true;
-                resolve(`The spaceship ${this.spaceship.name} is On. Actual charge: ${percent}%`);
-            } else {
-                reject(`Was not possible turn on the spaceship ${this.spaceship.name}. The charge is less than 30%. Percent: ${percent}%`);
-            }
-        });
+        if (percent >= 30) {
+            this.isOn = true;
+            return (`The spaceship ${this.spaceship.name} is On. Actual charge: ${percent}%`);
+        } else {
+            throw new Error(`Was not possible turn on the spaceship ${this.spaceship.name}. The charge is less than 30%. Percent: ${percent}%`);
+        }
     }
 
     async verifyShield() {
 
-        // Promise para verificar shield
-        return new Promise((resolve, reject) => {
-
-            let doubleShield = this.spaceship.shield * 2;
-            // Step1: Shield stronger enouth?
-            if (doubleShield < 100) {
-                reject(`Nave ${this.spaceship.name} - Shield máximo insuficiente: ${doubleShield}`);
-            };
-            // Reduce shield for 70% of the previous value
-            doubleShield *= 0.7;
-            // Step2: Shield out of range?
-            if (doubleShield > 120) {
-                reject(`Escudo em supercarga: ${doubleShield}`)
-            };
-            resolve(doubleShield);
-        });
+        let doubleShield = this.spaceship.shield * 2;
+        // Step1: Shield stronger enouth?
+        if (doubleShield < 100) {
+            throw new Error(`Nave ${this.spaceship.name} - Shield máximo insuficiente: ${doubleShield}`);
+        };
+        // Reduce shield for 70% of the previous value
+        doubleShield *= 0.7;
+        // Step2: Shield out of range?
+        if (doubleShield > 120) {
+            throw new Error(`Escudo em supercarga: ${doubleShield}`)
+        };
+        return (doubleShield);
     };
 };
