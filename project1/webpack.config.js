79a05a41ca97f8a62701c4dfc54fd1964e9bc224
taylorconde <taylorconde@gmail.com>
2025-03-22 15:42:01 -0300

    Saving Boilerplate

--- project1/webpack.config.js
+++ project1/webpack.config.js
@@ -1,36 +1,35 @@
-const { watchFile } = require("fs");
 const path = require("path");
 
 module.exports = {
     mode: "development",
 
     entry: {
-        index: './src/index.js',
+        index: './src/index.js', // Arquivo de entrada principal
     },
 
     output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
+        filename: '[name].bundle.js', // Nome do arquivo de saída
+        path: path.resolve(__dirname, "dist"), // Diretório de saída
+        clean: true, // Limpa o diretório de saída antes de gerar novos arquivos
     },
 
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
+                test: /\.js$/, // Testa todos os arquivos .js
+                exclude: /node_modules/, // Corrigido o erro de digitação
+                use: ['babel-loader'], // Utiliza o Babel para transpilar o código
             },
         ],
     },
 
     devServer: {
         static: {
-            directory: path.join(__dirname, "dist"),
+            directory: path.join(__dirname, "dist"), // Serve arquivos da pasta dist
         },
-        watchFiles: [path.join(__dirname, "src")],
-        liveReload: true,
-        open: true,
-        port: 8080,
+        watchFiles: [path.join(__dirname, "src")], // Monitora mudanças no diretório src
+        liveReload: true, // Atualização automática do navegador
+        open: true, // Abre o navegador automaticamente
+        port: 8080, // Porta para o servidor de desenvolvimento
     },
 };
