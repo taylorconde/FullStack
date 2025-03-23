f314f2a80c0a1dde8a172e0a7b70191216ce036d
taylorconde <taylorconde@gmail.com>
2025-03-23 18:13:47 -0300

    Adding exercicio2

---
+++ project2/src/objects.js
@@ -0,0 +1,8 @@
+export default class SentryObject {
+    constructor(id, name, minYears, maxYears) {
+        this.id = id;
+        this.name = name;
+        this.minYears = minYears;
+        this.maxYears = maxYears;
+    }
+}
